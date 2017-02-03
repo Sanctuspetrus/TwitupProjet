6f1388ae92158034250936b348801c607b51dffa
Makazy <heelomangola@gmail.com>
2017-02-03 11:51:33 +0100

    Interface de gestion des actions connexion/deconnexion

---
+++ src/com/iup/tp/twitup/ihm/login/TwitupAccountActionView.java
@@ -0,0 +1,22 @@
+package com.iup.tp.twitup.ihm.login;
+
+import com.iup.tp.twitup.ihm.TwitupView;
+import com.iup.tp.twitup.ihm.event.TwitupWatcher;
+
+public interface TwitupAccountActionView extends TwitupView{
+	
+	// Bouton Créer un compte
+	public void addActionSignUp(TwitupWatcher tw);
+	public void delActionSignUp(TwitupWatcher tw);
+	
+	// Bouton Connexion
+	public void addActionSignIn(TwitupWatcher tw);
+	public void delActionSignIn(TwitupWatcher tw);
+	public void showSignIn(boolean bool);
+	
+	// Bouton Déconnexion
+	public void addActionSignOut(TwitupWatcher tw);
+	public void delActionSignOut(TwitupWatcher tw);
+	public void showSignOut(boolean bool);
+	
+}
