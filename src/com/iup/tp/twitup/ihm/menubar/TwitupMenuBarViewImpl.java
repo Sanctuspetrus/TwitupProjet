01371e1f27e5571b0bb9b536bf4bf96986c823d3
Makazy <heelomangola@gmail.com>
2017-02-03 11:51:58 +0100

    Implementation actions connexions/deconnexions

--- src/com/iup/tp/twitup/ihm/menubar/TwitupMenuBarViewImpl.java
+++ src/com/iup/tp/twitup/ihm/menubar/TwitupMenuBarViewImpl.java
@@ -10,33 +10,51 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import com.iup.tp.twitup.ihm.event.TwitupWatchable;
 import com.iup.tp.twitup.ihm.event.TwitupWatcher;
+import com.iup.tp.twitup.ihm.login.TwitupAccountActionView;
 
-public class TwitupMenuBarViewImpl implements TwitupMenuBarView {
+public class TwitupMenuBarViewImpl implements TwitupMenuBarView, TwitupAccountActionView {
 
 	protected JFrame frame;
 
 	protected JMenuBar menuBar;
 	protected JMenu files;
+	protected JMenu account;
 	protected JMenu help;
 	protected JMenuItem shareFolder;
 	protected JMenuItem close;
 	protected JMenuItem about;
+	protected JMenuItem signUp;
+	protected JMenuItem signIn;
+	protected JMenuItem signOut;
 
 	protected TwitupWatchable closeWatcher;
 	protected TwitupWatchable aboutWatcher;
 	protected TwitupWatchable modifyExchangeFolderWatcher;
+	protected TwitupWatchable signUpWatcher;
+	protected TwitupWatchable signInWatcher;
+	protected TwitupWatchable signOutWatcher;
 
 	public TwitupMenuBarViewImpl(JFrame f) {
+		// Composants graphiques
 		menuBar = new JMenuBar();
 		frame = f;
 		files = new JMenu("Fichier");
+		account = new JMenu("Compte");
+		help = new JMenu("?");
 		shareFolder = new JMenuItem("Répertoire d'échange");
 		close = new JMenuItem("Quitter", new ImageIcon("src/resources/images/middle.gif"));
-		help = new JMenu("?");
 		about = new JMenuItem("A propos");
+		signUp = new JMenuItem("Créer un compte");
+		signIn = new JMenuItem("Connexion");
+		signOut = new JMenuItem("Déconnexion");
+		
+		// Watchers
 		closeWatcher = new TwitupWatchable();
 		aboutWatcher = new TwitupWatchable();
 		modifyExchangeFolderWatcher = new TwitupWatchable();
+		signUpWatcher = new TwitupWatchable();
+		signInWatcher = new TwitupWatchable();
+		signOutWatcher = new TwitupWatchable();
 	}
 
 	@Override
@@ -66,6 +84,43 @@ public class TwitupMenuBarViewImpl implements TwitupMenuBarView {
 		files.add(close);
 
 		menuBar.add(files);
+		
+		
+		// Menu Compte
+		account.setMnemonic(KeyEvent.VK_C);
+		// Bouton créer un compte
+		signUp.setMnemonic(KeyEvent.VK_P);
+		signUp.getAccessibleContext().setAccessibleDescription("Créer un nouveau compte utilisateur");
+		signUp.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				signUpWatcher.sendEvent();
+			}
+		});
+		account.add(signUp);
+		// Bouton de connexion
+		signIn.setMnemonic(KeyEvent.VK_P);
+		signIn.getAccessibleContext().setAccessibleDescription("Connexion");
+		signIn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				signInWatcher.sendEvent();
+			}
+		});
+		account.add(signIn);
+		// Bouton de déconnexion
+		signOut.setMnemonic(KeyEvent.VK_P);
+		signOut.getAccessibleContext().setAccessibleDescription("Déconnexion");
+		signOut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				signOutWatcher.sendEvent();
+			}
+		});
+		account.add(signOut);
+			
+		menuBar.add(account);
+		
 
 		// Build second menu in the menu bar.
 		help.setMnemonic(KeyEvent.VK_COMMA);
@@ -86,6 +141,23 @@ public class TwitupMenuBarViewImpl implements TwitupMenuBarView {
 		frame.setJMenuBar(menuBar);
 	}
 
+	public void close(){
+		// TODO
+		menuBar.removeAll();
+	}
+	
+	// ACTIONS
+	@Override
+	public void showSignIn(boolean bool) {
+		signIn.setVisible(bool);
+	}
+
+	@Override
+	public void showSignOut(boolean bool) {
+		signOut.setVisible(bool);
+	}
+	
+	// WATCHERS
 	@Override
 	public void addActionClose(TwitupWatcher tw) {
 		closeWatcher.addWatcher(tw);
@@ -115,5 +187,36 @@ public class TwitupMenuBarViewImpl implements TwitupMenuBarView {
 	public void delActionModifyExchangeFolder(TwitupWatcher tw) {
 		modifyExchangeFolderWatcher.delWatcher(tw);
 	}
+
+	@Override
+	public void addActionSignUp(TwitupWatcher tw) {
+		signUpWatcher.addWatcher(tw);
+	}
+
+	@Override
+	public void delActionSignUp(TwitupWatcher tw) {
+		signUpWatcher.delWatcher(tw);
+	}
+
+	@Override
+	public void addActionSignIn(TwitupWatcher tw) {
+		signInWatcher.addWatcher(tw);
+	}
+
+	@Override
+	public void delActionSignIn(TwitupWatcher tw) {
+		signInWatcher.delWatcher(tw);
+	}
+
+	@Override
+	public void addActionSignOut(TwitupWatcher tw) {
+		signOutWatcher.addWatcher(tw);
+	}
+
+	@Override
+	public void delActionSignOut(TwitupWatcher tw) {
+		signOutWatcher.delWatcher(tw);
+	}
+
 
 }
