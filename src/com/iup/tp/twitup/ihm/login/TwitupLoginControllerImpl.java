3c445b68be3c302587d897a537995a3963d4960e
razeltif <razeltif@gmail.com>
2017-02-03 11:32:30 +0100

    premiere implémentation de TwitupLogin

--- src/com/iup/tp/twitup/ihm/login/TwitupLoginControllerImpl.java
+++ src/com/iup/tp/twitup/ihm/login/TwitupLoginControllerImpl.java
@@ -1,25 +1,57 @@
 package com.iup.tp.twitup.ihm.login;
 
-import com.iup.tp.twitup.core.EntityManager;
+import java.util.Iterator;
+import java.util.Set;
+
+import com.iup.tp.twitup.datamodel.Database;
+import com.iup.tp.twitup.datamodel.User;
+import com.iup.tp.twitup.ihm.event.TwitupWatcher;
 
 public class TwitupLoginControllerImpl implements TwitupLoginController {
-	
-	protected EntityManager em;
+
+	protected Database db;
 	protected TwitupSignInView loginView;
-	
-	public TwitupLoginControllerImpl(EntityManager em, TwitupSignInView lv){
-		this.em = em;
+
+	public TwitupLoginControllerImpl(Database db, TwitupSignInView lv){
+		this.db = db;
 		this.loginView = lv;
 	}
-	
-	
-	
-	public boolean connexion(String pseudo, String password){
-		
-		
-		
-		
-		return false;		
+
+	public void initView(){
+		loginView.addActionLoginAttempt(loginAttempt());
+		loginView.show();
 	}
-	
+
+
+	//vérifie si l'utilisateur existe
+	//return l'User trouvé, null sinon
+	public User connection(String userTag, String password){
+
+		Set<User> users = db.getUsers();	
+		Iterator<User> i = users.iterator();
+		while(i.hasNext()){
+
+			//si le pseudo et mdp correspondent à un user, return du User
+			if(((i.next().getUserTag().equals(userTag)) && (i.next()).getUserPassword().equals(password))){
+				return i.next();
+			}
+		}
+		return null;		
+	}
+
+	public TwitupWatcher loginAttempt(){
+
+		return new TwitupWatcher() {
+			@Override
+			public void action(Object o) {
+				if(connection(loginView.getTag(), loginView.getPassword()) == null){
+					loginView.showError();
+				}else{
+					loginView.showSuccess();
+				}
+				
+			}
+		};
+	}
+
 }
--- src/com/iup/tp/twitup/ihm/login/TwitupSignInView.java
+++ src/com/iup/tp/twitup/ihm/login/TwitupSignInView.java
@@ -1,8 +1,21 @@
 package com.iup.tp.twitup.ihm.login;
 
 import com.iup.tp.twitup.ihm.TwitupView;
+import com.iup.tp.twitup.ihm.event.TwitupWatcher;
 
 public interface TwitupSignInView extends TwitupView {
+
+	void addActionConnection();
+
+	void showSuccess();
+
+	void showError();
+
+	void addActionLoginAttempt(TwitupWatcher loginAttempt);
+
+	String getTag();
+
+	String getPassword();
 
 	
 
