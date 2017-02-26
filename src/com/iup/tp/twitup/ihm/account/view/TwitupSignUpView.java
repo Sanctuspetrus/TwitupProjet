5b7fb4fc430cbfd1b5e5505c4251e05df179cc72
Heelo Mangola <heelomangola@gmail.com>
2017-02-26 21:12:47 +0100

    Ajout de AccountObserver sur LogOutView et SignUpView

--- src/com/iup/tp/twitup/ihm/account/view/TwitupSignUpView.java
+++ src/com/iup/tp/twitup/ihm/account/view/TwitupSignUpView.java
@@ -3,9 +3,10 @@ package com.iup.tp.twitup.ihm.account.view;
 import com.iup.tp.twitup.ihm.TwitupView;
 import com.iup.tp.twitup.ihm.account.TwitupError;
 import com.iup.tp.twitup.ihm.account.TwitupSuccess;
+import com.iup.tp.twitup.ihm.account.controller.AccountObserver;
 import com.iup.tp.twitup.ihm.event.TwitupWatcher;
 
-public interface TwitupSignUpView extends TwitupView, TwitupError, TwitupSuccess {
+public interface TwitupSignUpView extends TwitupView, AccountObserver, TwitupError, TwitupSuccess {
 
 	void addActionSignUp(TwitupWatcher tw);
 	void delActionSignUp(TwitupWatcher tw);
